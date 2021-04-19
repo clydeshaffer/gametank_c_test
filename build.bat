11bcc0f7dbf526902f92b0ab7ff5a7c3c908ffa6
Clyde Shaffer <clydeshaffer@gmail.com>
2021-04-19 09:59:14 -0400

    Tailor T0.ASM to GameTank mappings and replace startup.asm

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-WDC02AS startup.asm
+WDC02AS T0.ASM
 WDC02CC main.c
-WDCLN -HB -CE000 -D200, main.obj startup.obj -LC
+WDCLN -HB -CE000 -D200, main.obj T0.obj -LC -T
 dd bs=57344 skip=1 if=main.bin of=main.gtr
